ea8b311c66448278912c04f12afa3fa4fbdd21e9
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-06 18:12:40 +0700

    chore: setup supabase and shadCN-ui

---
+++ src/types/INews.d.ts
@@ -0,0 +1,11 @@
+export interface INews {
+  id: string;
+  title: string;
+  slug: string;
+  content: string;
+  author: string;
+  categories: string;
+  thumbnile: string;
+  created_at: string;
+  updated_at: string;
+}
