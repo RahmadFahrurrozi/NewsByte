09a70df814299240dd7196a75db7424f6217e05f
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-25 14:34:52 +0700

    reafactor: move theme provider into context folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavbarWrapper from "@/components/Navbar/NavbarWrapper";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 const poppins = Poppins({
   subsets: ["latin"],
