ae26c95596c2f44b88b4b68b6aaabf5509905fdc
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-28 21:58:09 +0700

    feat: enhance login and registration pages with back navigation link

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { LoginForm } from "@/components/LoginForm";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function LoginPage() {
   const handleSuccess = () => {};
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <LoginForm onSuccess={handleSuccess} />
-    </div>
+    <section className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col gap-4">
+        <Link href="/">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <div className="rounded-full p-2 border border-border bg-background/80 backdrop-blur-md shadow w-fit">
+              <ArrowLeft className="size-4 text-foreground" />
+            </div>
+            <p className="text-muted-foreground">Back</p>
+          </div>
+        </Link>
+        <LoginForm onSuccess={handleSuccess} />
+      </div>
+    </section>
   );
 }
