cbb6c9ca8e993a1d83040fba49f584e1fcf42054
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-31 10:54:58 +0700

    feat: wrap AppSidebarUser component with ProtectedRoute for role-based access control

--- src/components/AppSidebarUser.tsx
+++ src/components/AppSidebarUser.tsx
@@ -10,20 +10,23 @@ import { NavMain } from "@/components/NavMain";
 import { NavUser } from "@/components/NavUser";
 import { TeamSwitcher } from "@/components/team-switcher";
 import { sidebarUser } from "@/constants/dataSideBar";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export function AppSidebarUser(props: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <TeamSwitcher teams={sidebarUser.teams} />
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={sidebarUser.nav} />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={sidebarUser.user} />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
+    <ProtectedRoute allowedRoles={["user"]}>
+      <Sidebar collapsible="icon" {...props}>
+        <SidebarHeader>
+          <TeamSwitcher teams={sidebarUser.teams} />
+        </SidebarHeader>
+        <SidebarContent>
+          <NavMain items={sidebarUser.nav} />
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser user={sidebarUser.user} />
+        </SidebarFooter>
+        <SidebarRail />
+      </Sidebar>
+    </ProtectedRoute>
   );
 }
