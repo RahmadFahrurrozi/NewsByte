398116a9e2d50ce102fc5bf5695dfd6b0a7fe53a
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-31 10:50:41 +0700

    feat: replace loading animation on Unauthorized page with CCTVCamera.lottie and add back button functionality

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
+import { useRouter } from "next/navigation";
 export default function Unauthorized() {
+  const router = useRouter();
   return (
     <div className="flex items-center flex-col justify-center min-h-screen">
-      <DotLottieReact src="/loading.lottie" loop autoplay className="w-96" />
-      <h1 className="text-2xl font-bold">Hold up</h1>
+      <DotLottieReact src="/CCTVCamera.lottie" loop autoplay className="w-96" />
+      <h1 className="text-2xl font-bold">Upps!</h1>
       <p className="text-gray-600">
         You are not authorized to access this page.
       </p>
+      <Button
+        className="mt-4 cursor-pointer"
+        variant="outline"
+        onClick={() => router.back()}
+      >
+        Back
+      </Button>
     </div>
   );
 }
