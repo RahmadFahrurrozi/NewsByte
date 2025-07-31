38da50a4a415e9f4af46b44f505cc1b5c503f634
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-07-31 10:51:22 +0700

    fix: update error messages in auth schemas for clarity

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email({ message: "email is required" }),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
 export const registerSchema = z
   .object({
-    name: z.string().min(2, "Name must be at least 2 characters"),
-    email: z.string().email({ message: "Invalid email address" }),
+    username: z.string().min(2, "Name must be at least 2 characters"),
+    email: z.string().email({ message: "email is required" }),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z
       .string()
