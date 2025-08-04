238d5f482b109961b8c63d77b646bfb71531334d
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-04 10:23:56 +0700

    fix: refomve duplicat message error

--- src/services/login.ts
+++ src/services/login.ts
@@ -13,28 +13,15 @@ interface ILoginResponse {
 export default async function loginUser(
   payload: LoginFormValues
 ): Promise<ILoginResponse> {
-  try {
-    const response = await fetch("/api/auth/login", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+  const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
 
-    const result: ILoginResponse = await response.json();
+  const result: ILoginResponse = await response.json();
 
-    if (!response.ok || !result.success) {
-      const errorMessage = result.error || "Login failed";
-      throw new Error(errorMessage);
-    }
-
-    return result;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error(error.message || "Login failed");
-    }
-
-    throw new Error("Internal server error");
-  }
+  return result;
 }
--- src/services/register.ts
+++ src/services/register.ts
@@ -9,28 +9,15 @@ interface IRegisterResponse {
 export default async function regsiterUser(
   payload: RegisterFormValues
 ): Promise<IRegisterResponse> {
-  try {
-    const response = await fetch("/api/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+  const response = await fetch("/api/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
 
-    const result: IRegisterResponse = await response.json();
+  const result: IRegisterResponse = await response.json();
 
-    if (!response.ok || !result.success) {
-      const errorMessage = result.error || "Registration failed";
-      throw new Error(errorMessage);
-    }
-
-    return result;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error(error.message || "Registration failed");
-    }
-
-    throw new Error("Internal server error");
-  }
+  return result;
 }
