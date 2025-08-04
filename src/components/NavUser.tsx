39a25e8cf5a7e9b549c8e7a6cc2e5dcd26d85284
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-04 10:19:59 +0700

    feat: adding initial alpabhet for image profile user and fixed other component

--- src/components/NavUser.tsx
+++ src/components/NavUser.tsx
@@ -4,9 +4,9 @@ import {
   BadgeCheck,
   Bell,
   ChevronsUpDown,
-  CreditCard,
   LogOut,
   Sparkles,
+  User,
 } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -28,10 +28,10 @@ import {
 import { Button } from "./ui/button";
 import { useLogout } from "@/hooks/useLogout";
 import { LoadingSpinner } from "./LoadingSpinner";
+import { useAuth } from "@/contexts/AuthContextProvider";
+import { Badge } from "./ui/badge";
 
-export function NavUser({
-  user,
-}: {
+export function NavUser({}: {
   user: {
     name: string;
     email: string;
@@ -40,6 +40,27 @@ export function NavUser({
 }) {
   const { isMobile } = useSidebar();
   const { handleLogout, loading } = useLogout();
+  const { user, userRole } = useAuth();
+
+  // function helper to get initials from email
+  const getInitials = (email: string | undefined) => {
+    if (!email) return "";
+    const parts = email.split("@");
+    if (parts.length > 0) {
+      return parts[0].charAt(0).toUpperCase();
+    }
+    return "";
+  };
+
+  const getUsernameFromEmail = (email: string | undefined) => {
+    if (!email) return "Pengguna";
+    // Ambil bagian sebelum @
+    const username = email.split("@")[0];
+    // Ubah huruf pertama jadi kapital
+    return username.charAt(0).toUpperCase() + username.slice(1);
+  };
+
+  const initials = getInitials(user?.email);
 
   return (
     <SidebarMenu>
@@ -50,13 +71,16 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <Avatar>
+                <AvatarFallback className="text-foreground">
+                  {initials}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">
+                  {getUsernameFromEmail(user?.email)}
+                </span>
+                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -69,13 +93,24 @@ export function NavUser({
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <Avatar>
+                  <AvatarFallback className="text-foreground">
+                    {initials}
+                  </AvatarFallback>
                 </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                <div className="flex items-center gap-2 text-sm leading-tight truncate">
+                  <span className="truncate font-medium">
+                    {getUsernameFromEmail(user?.email)}
+                  </span>
+                  <Badge
+                    className={`truncate text-xs ${
+                      userRole === "admin"
+                        ? "bg-red-100 text-red-500"
+                        : "bg-blue-100 text-blue-500"
+                    }`}
+                  >
+                    {userRole}
+                  </Badge>
                 </div>
               </div>
             </DropdownMenuLabel>
@@ -104,14 +139,16 @@ export function NavUser({
               disabled={loading}
               className="cursor-pointer my-1"
             >
-              <LogOut />
               {loading ? (
                 <>
                   <LoadingSpinner className="mr-2" />
-                  Logging out...
+                  <p>Loging out...</p>
                 </>
               ) : (
-                "Log out"
+                <>
+                  <LogOut />
+                  <p>Log out</p>
+                </>
               )}
             </Button>
           </DropdownMenuContent>
