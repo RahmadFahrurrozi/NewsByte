5ebdbd8e99e74fbea2b189657be1c0f91aa4606a
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-04 10:25:22 +0700

    feat: display username at dashboard main user

--- src/app/dashboard-user/page.tsx
+++ src/app/dashboard-user/page.tsx
@@ -1,8 +1,16 @@
-export default function DashboardMain() {
+"use client";
+
+import { useAuth } from "@/contexts/AuthContextProvider";
+
+export default function DashboardMainUser() {
+  const { username } = useAuth();
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-3">Dashboard User</h1>
-      <p className="text-gray-600">Loading user dashboard...</p>
+      <p className="text-gray-300">
+        Welcome, <span className="text-blue-500 font-semibold">{username}</span>
+      </p>
     </div>
   );
 }
