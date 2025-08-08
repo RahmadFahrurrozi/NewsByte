9ee4f20dd9b585ab9720375c8327954af041eeba
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-09 05:55:16 +0700

    feat: create component empty article and fix import protectedroute

--- src/components/EmptyArticleClient.tsx
+++ src/components/EmptyArticleClient.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import Link from "next/link";
-import { Button } from "./ui/button";
 
-export default function EmptyNewsClient() {
+export default function EmptyArticleClient() {
   return (
     <div className="flex items-center flex-col justify-center min-h-[80vh]">
-      <DotLottieReact src="/CCTVCamera.lottie" loop autoplay className="w-96" />
+      <DotLottieReact
+        src="/notFound.lottie"
+        loop
+        autoplay
+        className="w-[500px]"
+      />
       <h1 className="text-2xl font-bold">Upps!</h1>
       <p className="text-gray-600">No news found!, waiting for new aricles</p>
-      <Button className="mt-4 cursor-pointer">Back to home</Button>
     </div>
   );
 }
