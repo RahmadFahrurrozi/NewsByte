67b1d38fb238bd082b4d1373a9899e3df5968661
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-11 19:40:07 +0700

    feat: add interface and implement pagination logic backend

--- src/lib/article/getApprovedArticles.ts
+++ src/lib/article/getApprovedArticles.ts
@@ -1,16 +1,24 @@
 import { createClient } from "@/lib/supabase/client";
 import { IArticles } from "@/types/IArticles";
+import { getPagination } from "@/utils/getPagination";
 
-export async function getApprovedArticles(): Promise<{
+interface IArticlesApproved {
   data: IArticles[] | null;
   error: Error | null;
-}> {
+}
+
+export async function getApprovedArticles(
+  page: number,
+  perPage: number
+): Promise<IArticlesApproved> {
   const supabase = createClient();
+  const { from, to } = getPagination(page, perPage);
 
   const { data, error } = await supabase
     .from("articles")
     .select(`*, profiles(*)`)
     .eq("article_status", "approved")
+    .range(from, to)
     .order("created_at", { ascending: false });
 
   return { data, error };
