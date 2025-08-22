c9d966db11ea3ab3b3f64f3c93ec47f3f2a5123e
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-22 12:58:54 +0700

    feat: adding supabase chace helper and fix proveder react query

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
+import { ReactNode, useState } from "react";
 
 export default function ReactQueryProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      {/* {process.env.NODE_ENV === "development" && (
-        <ReactQueryDevtools initialIsOpen={false} position="right" />
-      )} */}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
