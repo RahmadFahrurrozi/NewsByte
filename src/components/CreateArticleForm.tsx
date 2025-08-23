9b8d1f314b875506746c1d8e6f2effe07191cb55
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-08-24 06:29:26 +0700

    feat: create article form

---
+++ src/components/CreateArticleForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import useCreateArticle from "@/hooks/useCreateArticle";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { LoadingSpinner } from "./LoadingSpinner";
+
+export default function CreateArticleForm() {
+  const { form, handleSubmit, isLoading } = useCreateArticle();
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Title" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the title of your article.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="thumbnile"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Thumbnile</FormLabel>
+              <FormControl>
+                <Input placeholder="Thumbnile" {...field} />
+              </FormControl>
+              <FormDescription>
+                URL of the thumbnile image for your article.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Content" {...field} />
+              </FormControl>
+              <FormDescription>
+                The main content of your article.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="categories"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Categories</FormLabel>
+              <FormControl>
+                <Input placeholder="Categories" {...field} />
+              </FormControl>
+              <FormDescription>
+                Categories related to your article.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            variant={"outline"}
+            className="cursor-pointer"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <>
+                <LoadingSpinner className="mr-2" />
+                Submitting...
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
