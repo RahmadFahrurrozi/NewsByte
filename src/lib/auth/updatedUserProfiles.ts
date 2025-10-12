9a7b486e5845d418ed0dbdcc46346661971114cb
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-12 20:31:03 +0700

    feat: add backend logic for update user profile and upload photo profile picture

---
+++ src/lib/auth/updatedUserProfiles.ts
@@ -0,0 +1,34 @@
+import { createClient } from "../supabase/client";
+import { IUserProfile } from "@/types/IUserProfile";
+
+const supabase = createClient();
+
+export async function uploadProfilePicture(file: File) {
+  const fileExt = file.name.split(".").pop();
+  const fileName = `${Date.now()}-${file.name}`;
+  const filePath = `profileUser/${fileName}.${fileExt}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from("profile-picture")
+    .upload(filePath, file);
+
+  if (uploadError) throw uploadError;
+
+  const { data: publicUrl } = supabase.storage
+    .from("profile-picture")
+    .getPublicUrl(filePath);
+
+  return publicUrl.publicUrl;
+}
+
+export async function updatedUserProfiles(profile: IUserProfile) {
+  const { data, error } = await supabase
+    .from("profiles")
+    .update(profile)
+    .eq("id", profile.id)
+    .select()
+    .single();
+
+  if (error) throw new Error(error.message);
+  return data;
+}
