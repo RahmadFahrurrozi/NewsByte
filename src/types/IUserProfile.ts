da1abdb06255c8b91a0dd5408d1f520e8f3939f1
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-12 20:29:36 +0700

    feat: add some interface at user profile

--- src/types/IUserProfile.ts
+++ src/types/IUserProfile.ts
@@ -2,9 +2,15 @@ export interface IUserProfile {
   id: string;
   name: string;
   username: string;
-  photo: string;
-  bio: string;
-  link: string;
+  photo?: string;
+  bio?: string;
+  link?: string;
+  website?: string;
+  medium?: string;
+  linkedin?: string;
+  twitterx?: string;
+  account_status?: string;
+  profile_completion?: number;
   email: string;
   role: string;
   created_at: string;
