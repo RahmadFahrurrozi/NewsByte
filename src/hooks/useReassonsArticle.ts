1136e1b013ac086a5775b1cf23679653629e103e
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-11-04 10:04:51 +0700

    feat: enhance useArticleReassons hook to include article rejection logic with user authentication and success/error handling

--- src/hooks/useReassonsArticle.ts
+++ src/hooks/useReassonsArticle.ts
@@ -1,11 +1,22 @@
+"use client";
+
+import { useAuth } from "@/contexts/AuthContextProvider";
+import { rejectArticleWithReasson } from "@/lib/article/reassonArticle";
 import {
   reassonsArtcleSchema,
   reassonsArtcleValues,
 } from "@/schemas/reassonsArticle.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
-export default function useArticleReassons() {
+export default function useArticleReassons(articleId: string) {
+  const router = useRouter();
+  const { user } = useAuth();
+  const queryClient = useQueryClient();
+
   const form = useForm<reassonsArtcleValues>({
     resolver: zodResolver(reassonsArtcleSchema),
     defaultValues: {
@@ -14,17 +25,47 @@ export default function useArticleReassons() {
     },
   });
 
+  const rejectMutation = useMutation({
+    mutationFn: async (data: reassonsArtcleValues) => {
+      if (!articleId || !user?.id) {
+        throw new Error("Article ID or user ID is not available.");
+      }
+      await rejectArticleWithReasson(articleId, data, user.id);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["pending-articles"] });
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
+      toast.success("Article rejected successfully!");
+      router.push(`/dashboard-admin/approval-articles`);
+    },
+    onError: (error: Error) => {
+      console.error("Error rejecting article:", error);
+      toast.error(`Failed to reject article: ${error.message}`);
+    },
+  });
+
   const onSubmit = (data: reassonsArtcleValues) => {
-    console.log("Reasson Article Data:", data);
+    if (!articleId) {
+      toast.error("Article ID is missing");
+      return;
+    }
+    if (!user?.id) {
+      toast.error("You Must Login First");
+      return;
+    }
+    rejectMutation.mutate(data);
   };
 
   const handleCancle = () => {
     form.reset();
+    router.push(`/dashboard-admin/approval-articles`);
   };
 
   return {
     form,
     onSubmit,
     handleCancle,
+    isLoading: rejectMutation.isPending,
+    articleId,
   };
 }
