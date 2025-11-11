5dcb2f8b4f103e60c9611aadbb2c0b36a73c3331
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-11-11 14:24:19 +0700

    feat: add getTotalRejectedStats function to retrieve total count of rejected articles for authenticated users

--- src/services/article/getRejectedArticle.ts
+++ src/services/article/getRejectedArticle.ts
@@ -6,6 +6,11 @@ interface IRejectedArticles {
   error: Error | null;
 }
 
+interface IGetRejecedStats {
+  totalRejected: number;
+  error?: Error | null;
+}
+
 export async function getRejectedArticles(): Promise<IRejectedArticles> {
   const supabase = createClient();
   const {
@@ -33,4 +38,35 @@ export async function getRejectedArticles(): Promise<IRejectedArticles> {
   }
 
   return { data, error };
+}
+
+export async function getTotalRejectedStats(): Promise<{
+  data: IGetRejecedStats | null;
+  error: Error | null;
+}> {
+  const supabase = createClient();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError) {
+    throw new Error(`Failed to get user: ${userError.message}`);
+  }
+
+  const { count, error } = await supabase
+    .from("articles")
+    .select("id", { count: "exact" })
+    .eq("article_status", "rejected")
+    .eq("author_id", user?.id);
+
+  if (error) {
+    throw new Error(`Failed to get total rejected articles: ${error.message}`);
+  }
+
+  const stats: IGetRejecedStats = {
+    totalRejected: count || 0,
+  };
+
+  return { data: stats, error: null };
 }
