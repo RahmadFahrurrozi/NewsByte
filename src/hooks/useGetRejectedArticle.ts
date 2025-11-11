23d2f7113298d302d0dc9cda1612f5876907534b
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-11-11 14:24:28 +0700

    feat: add useGetTotalRejectedCount hook to fetch total count of rejected articles

--- src/hooks/useGetRejectedArticle.ts
+++ src/hooks/useGetRejectedArticle.ts
@@ -1,10 +1,23 @@
-import { getRejectedArticles } from "@/services/article/getRejectedArticle";
+import {
+  getRejectedArticles,
+  getTotalRejectedStats,
+} from "@/services/article/getRejectedArticle";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const useGetRejectedArticles = () => {
   return useQuery({
     queryKey: ["rejected-articles"],
     queryFn: () => getRejectedArticles(),
+    placeholderData: keepPreviousData,
+    staleTime: 5 * 60 * 1000,
+    gcTime: 10 * 60 * 1000,
+  });
+};
+
+export const useGetTotalRejectedCount = () => {
+  return useQuery({
+    queryKey: ["total-rejected-count"],
+    queryFn: () => getTotalRejectedStats(),
     placeholderData: keepPreviousData,
     staleTime: 5 * 60 * 1000,
     gcTime: 10 * 60 * 1000,
